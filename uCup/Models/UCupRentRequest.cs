7f2b52f15d300ccedb8a360039c552e530877d98
Elton Hsiao Yu Teng <elton.hsiao@titansoft.com.sg>
2021-12-23 22:34:12 +0800

    add POST to do rent

---
+++ uCup/Models/UCupRentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace uCup.Models
+{
+    public class UCupRentRequest
+    {
+        public string UniqueId { get; set; }
+        public string MerchantCode { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
